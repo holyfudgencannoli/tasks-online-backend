f1713c105ffc590cdd36f67f291ac46285ddbdb4
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-09-04 13:18:30 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    DATABASE_URL = "sqlite:///var/data/react-tasks-online.db"
+    DATABASE_URL = "sqlite:///react-tasks-online.db"
     SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "jwtsecretkey"
+
 
