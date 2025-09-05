7880839e39911efa8485fe1665f754f66e207eec
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-09-05 11:13:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
-    DATABASE_URL = "sqlite:///react-tasks-online.db"
+    DATABASE_URL = "postgresql://tasks_online_db_user:PmcrEw9xtnnzy5bEdhSjOaIwSVrC9KnT@dpg-d2t65puuk2gs73ci59v0-a/tasks_online_db"
     SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "jwtsecretkey"
+
 
 
