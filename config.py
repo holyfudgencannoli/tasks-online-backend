9dee63b1fc9125c13b0add142f9b4904887fb7e6
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-10-06 12:25:21 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
 class Config:
-    DATABASE_URL = "postgresql://tasks_online_db_v878_user:RQqQwxWao2uJBzS8j1O8fDwuEuwQnzcO@dpg-d2tl21ndiees738bsb3g-a:5432/tasks_online_db_v878"
+    DATABASE_URL = "postgresql://task_docket_db_user:F1nmYKc4C6QVgK66shdoV5ioceBtOWla@dpg-d3hughe3jp1c73fs9ln0-a/task_docket_db"
     SECRET_KEY = os.environ.get("SECRET_KEY") or "a4bc9ded472bf5s4d5f4s54f88e74rw41sd59t"
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "793db7be3b7ch4t85g4u8x5sd476fsd5j47y8j"
     
     
+
 
 
 
